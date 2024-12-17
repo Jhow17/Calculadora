e9625027c2c4edd1db21cde4a28dbf42ab244da3
Jhow17 <silvajonathan234@gmail.com>
2024-12-17 10:27:19 -0300

    feat: Adicionar a execução das 4 operaçoes dos botoes CE, C, e DEL

--- js/script.js
+++ js/script.js
@@ -13,6 +13,7 @@ class Calculator{
     }
     // adiciona os na tela
     addDigit(digit){
+
         if (digit === "." && this.currentOperationText.innerText.includes(".")){
             return;
         }
@@ -21,15 +22,48 @@ class Calculator{
     }
     // Process all calculator operations
     proceessOperation(operation){
+        if (this.currentOperationText.innerText === "" && operation !== "C"){
+            // Change Operation
+            if(previousOperationText !== ""){
+                this.changeOperation(operation);
+            }
+            return;
+        }
+
         // Get current and previus value
         let operationValue ;
-        const previous = +this.previousOperationText.innerText;
-        const current = +this.currentOperation.innerText;
-        switch(operation){
-            case "+":
-                operationValue = previous + current
-                this.updateUpscreen(operationValue, operation, current, previous)
+        const previous = +this.previousOperationText.innerText.split(" ")[0];
+        const current = +this.currentOperationText.innerText;
 
+        switch(operation){
+
+            case ":":
+                operationValue = previous / current;
+                this.updateUpscreen(operationValue, operation, current, previous);
+                break;
+            case "-":
+                operationValue = previous - current;
+                this.updateUpscreen(operationValue, operation, current, previous);
+                break;
+            case "x":
+                operationValue = previous * current;
+                this.updateUpscreen(operationValue, operation, current, previous);
+                break;
+            case "+":
+                operationValue = previous + current;
+                this.updateUpscreen(operationValue, operation, current, previous);
+                break;
+            case "DEL": 
+                this.processDelOperator();
+                break;
+            case "CE":
+                this.processClearCurrentOperation();
+                break;
+            case "C":
+                this.processClearOperation();
+                break;
+            case "=":
+                this.processEqualOperator();
                 break;
             default:
                 return;
@@ -44,10 +78,67 @@ class Calculator{
         if(operationValue === null){
             this.currentOperationText.innerText += this.currentOperation;
         } else{
-            // check if value is zero
-        }
-        
+            // check if value is zero if it is jus add current value
+            if (previous === 0){
+                operationValue = current
+            }
+            if (operation === "="){
+                this.currentOperationText.innerText = `${operationValue}`;
+                this.previousOperationText.innerText = "";
+            }
+            else{
+            // Add current value to previus
+            this.previousOperationText.innerText = `${operationValue} ${operation}`;
+            this.currentOperationText.innerText = '';
+            }
 
+        }
+
+    }
+    changeOperation(operation){
+        const mathOperations = ["x",':','+','-']
+        if(!mathOperations.includes(operation)){
+            return
+        }
+        this.previousOperationText.innerText = this.previousOperationText.innerText.slice(0, -1) + operation
+
+    }
+    // delete the last digit
+    processDelOperator(){
+        this.currentOperationText.innerText = this.currentOperationText.innerText.slice(0, -1)
+    }
+    processClearCurrentOperation(){
+        this.currentOperationText.innerText = ""
+    }
+    processClearOperation(){
+        this.currentOperationText.innerText = ""
+        this.previousOperationText.innerText = ""
+    }
+    processEqualOperator() {
+        const operation = this.previousOperationText.innerText.split(" ")[1];
+        const previous = +this.previousOperationText.innerText.split(" ")[0];
+        const current = +this.currentOperationText.innerText;
+    
+        let operationValue;
+    
+        switch(operation) {
+            case "+":
+                operationValue = previous + current;
+                break;
+            case "-":
+                operationValue = previous - current;
+                break;
+            case "x":
+                operationValue = previous * current;
+                break;
+            case ":":
+                operationValue = previous / current;
+                break;
+            default:
+                return;
+        }
+    
+        this.updateUpscreen(operationValue, "="); // Atualiza a tela com o resultado
     }
 
 }
